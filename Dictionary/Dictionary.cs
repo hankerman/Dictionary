47308a4e58abf65a1f8b29c49388f4faf672a941
Hankerman <stalker-2204@mail.ru>
2022-10-02 13:50:31 +0300

    Добавьте файлы проекта.

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    internal class Dictionarys
+    {
+
+        SortedDictionary<string, string> dictionary;
+
+        public Dictionarys()
+        {
+            dictionary = new SortedDictionary<string, string>();
+            using (FileStream fs = new FileStream("dicti.txt", FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                    if(!sr.EndOfStream)
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string temp = sr.ReadLine();
+                            string[] tmp = temp.Split('|');
+                            dictionary.Add(tmp[0], tmp[1]);
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        public void Add(string word, string translation)
+        {
+
+            if (dictionary.ContainsKey(word))
+            {
+                string temp = dictionary[word];
+                temp += ", " + translation;
+                dictionary[word] = temp;
+            }
+            else
+            {
+                dictionary.Add(word, translation);
+            }
+            
+        }
+
+        public void Save()
+        {
+
+            using (FileStream fs = new FileStream("dicti.txt", FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write))
+            {
+
+                using (StreamWriter sr = new StreamWriter(fs))
+                {
+
+                    foreach(var it in dictionary)
+                    {
+
+                        sr.WriteLine($"{it.Key}|{it.Value}");
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        public void Print()
+        {
+
+            foreach (var it in dictionary)
+            {
+
+                Console.WriteLine($"{it.Key}\t|  {it.Value}");
+
+            }
+
+        }
+
+        public void Del(string word)
+        {
+            dictionary.Remove(word);
+        }
+
+        public void Edit()
+        {
+
+            DialogResult res = MessageBox.Show("Редактировать слово?", "Редактор", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if(res == DialogResult.Yes)
+            {
+                Console.WriteLine("Введите слово которое хотите отредактировать");
+                string tempKey = Console.ReadLine();
+                string tempValue = dictionary[tempKey];
+                Console.WriteLine("Введите слово отредактированым");
+                string newKey = Console.ReadLine();
+                dictionary.Remove(tempKey);
+                dictionary.Add(newKey, tempValue);
+            }
+            else if(res == DialogResult.No)
+            {
+
+                Console.WriteLine("Введите слово значение которого хотите отредактировать");
+                string tempKey = Console.ReadLine();
+                Console.WriteLine("Введите новые значения через запятую");
+                string tempValue = Console.ReadLine();
+                dictionary[tempKey] = tempValue;
+
+            }
+            else
+            {
+
+                Console.WriteLine("Не редактируем");
+
+            }
+
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionarys dictionary = new Dictionarys();
+            //dictionary.Add("square", "квадрат");
+            //dictionary.Add("square", "площадь");            
+            dictionary.Print();
+            dictionary.Edit();
+            dictionary.Print();
+            dictionary.Save();
+
+        }
+    }
+}
